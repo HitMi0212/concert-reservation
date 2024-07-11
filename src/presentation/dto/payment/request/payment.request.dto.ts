8d940cf55c8725f5f53d7780e6b994f846bd7c87
Seung Jun <pache31@naver.com>
2024-07-11 23:55:32 +0900

    [Feat] 결제 API Swagger 생성

--- src/presentation/dto/payment/request/payment.request.dto.ts
+++ src/presentation/dto/payment/request/payment.request.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsUUID } from 'class-validator';
 
 export class PaymentRequstDto {
+  @ApiProperty()
   @IsUUID()
   userId: string;
 
+  @ApiProperty()
   @IsNumber()
   reservationId: number;
 
+  @ApiProperty()
   @IsNumber()
   tokenId: number;
 
