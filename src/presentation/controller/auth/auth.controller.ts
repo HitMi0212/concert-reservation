849455d32292b347c26e2f38f5331171f916b461
Seung Jun <pache31@naver.com>
2024-07-11 23:55:10 +0900

    [Feat] 토큰 API Swagger 생성

--- src/presentation/controller/auth/auth.controller.ts
+++ src/presentation/controller/auth/auth.controller.ts
@@ -2,11 +2,17 @@ import { Body, Controller, Get, Headers, Patch, Post } from '@nestjs/common';
 import { TokenStatusEnum } from '../../dto/auth/enum/auth.enum';
 import { TokenRequestDto } from '../../dto/auth/request/auth.request.dto';
 import { TokenResponseDto } from '../../dto/auth/response/auth.response.dto';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('토큰 API')
 export class AuthController {
   // constructor(private readonly queueService: QueueService) {}
 
+  @ApiOperation({
+    summary: '대기열 토큰 생성',
+  })
+  @ApiOkResponse({ type: TokenResponseDto })
   @Post('/token')
   async createToken(
     @Body() requestBody: TokenRequestDto,
@@ -20,6 +26,10 @@ export class AuthController {
     });
   }
 
+  @ApiOperation({
+    summary: '대기열 토큰 연장',
+  })
+  @ApiOkResponse({ type: TokenResponseDto })
   @Patch('/token/extension')
   async extensionToken(
     @Headers('authorization') tokenId: number,
@@ -33,6 +43,10 @@ export class AuthController {
     });
   }
 
+  @ApiOperation({
+    summary: '대기열 토큰 검증',
+  })
+  @ApiOkResponse({ type: TokenResponseDto })
   @Get('/token/validation')
   async validateToken(
     @Headers('authorization') tokenId: number,
@@ -46,6 +60,10 @@ export class AuthController {
     });
   }
 
+  @ApiOperation({
+    summary: '대기열 토큰 만료',
+  })
+  @ApiOkResponse({ type: TokenResponseDto })
   @Patch('/token/expire')
   async expireToken(
     @Headers('authorization') tokenId: number,
--- src/presentation/dto/auth/response/auth.response.dto.ts
+++ src/presentation/dto/auth/response/auth.response.dto.ts
@@ -1,19 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDate, IsEnum, IsNumber, IsUUID } from 'class-validator';
 import { TokenStatusEnum } from '../enum/auth.enum';
 
 export class TokenResponseDto {
+  @ApiProperty()
   @IsNumber()
   id: number;
 
+  @ApiProperty()
   @IsUUID()
   userId: string;
 
+  @ApiProperty()
   @IsNumber()
   concertId: number;
 
+  @ApiProperty()
   @IsEnum(TokenStatusEnum)
   status: TokenStatusEnum;
 
+  @ApiProperty()
   @IsDate()
   expiredAt: Date;
 
